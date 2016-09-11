3c5c7406b4de3ab08bd0af08cce536c644dd66b3
Chuck Harmston <chuck@mozilla.com>
2016-09-11 17:01:12 -0600

    Handle server errors by passing on response body (closes #5).

--- aethervial.js
+++ aethervial.js
@@ -8,13 +8,11 @@ module.exports = function(str) {
     fuzzy: str,
     format: 'text'
   }).toString();
-  console.log(2, 'Promise init', str, url);
   return new Promise(function(resolve, reject) {
-    console.log(3, 'Make request', url);
     request(url).then(response => {
       resolve(response);
     }).catch(err => {
-      reject(err);
+      resolve(err.response.body);
     });
   });
 }
