b297cde3d30541dcdb25ff281b043d6079f08f6a
Chuck Harmston <chuck@mozilla.com>
2016-09-11 16:08:13 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+# thopter
+
+Slackbot to unfurl MTG cards from [aethervial.net](https://www.aethervial.net).
+
+
+## Setup
+
+```
+npm install
+node ./index.js
+```
---
+++ index.js
@@ -0,0 +1,38 @@
+const aethervial = require('./aethervial');
+const boldfirst = require('./middleware/boldfirst');
+const Botkit = require('botkit');
+const manamoji = require('./middleware/manamoji');
+const urlencode = require('urlencode');
+
+const PATTERN = /\[\[([^\]]+)\]\]/g;
+const TYPES = ['ambient', 'direct_message', 'direct_mention', 'mention', 'message_received'];
+
+if (!process.env.SLACK_TOKEN) {
+  console.log('Error: Specify token in environment');
+  process.exit(1);
+}
+
+const controller = Botkit.slackbot({
+  debug: true
+});
+
+controller.spawn({
+  token: process.env.SLACK_TOKEN
+}).startRTM(err => {
+  if (err) throw new Error(err);
+});
+
+controller.hears([PATTERN], TYPES, (bot, message) => {
+  let promises = [];
+  message.match.forEach(match => {
+    promises.push(new Promise((resolve, reject) => {
+      let cardname = match.substring(0, match.length - 2).substring(2);
+      aethervial(cardname).then(card => {
+        resolve(boldfirst(manamoji(card)));
+      });
+    }));
+  });
+  Promise.all(promises).then(cards => {
+    bot.reply(message, cards.join('\n\n'));
+  });
+});
